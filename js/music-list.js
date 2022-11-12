15a515f0498a8191b535ef696cf5a5b9431f107c
Nguyễn Đức Tài <95633728+Ngdtai2k2@users.noreply.github.com>
2022-11-12 19:17:28 +0700

    super update

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+
+let allMusic = [
+  {
+    name: "Bingo",
+    artist: "Nguyên ft. Mc Wiz & Boyzed",
+    img: "music-1", //bingo
+    src: "music-1"
+  },
+  {
+    name: "Lạc Vào Trong Mơ ",
+    artist: "Simon C ft. Wuy",
+    img: "music-9",//lvtm
+    src: "music-9"
+  },
+  {
+    name: "PRECIOUS",
+    artist: "DADUC ft. KIPER T",
+    img: "music-2",// precious
+    src: "music-2"
+  },
+  {
+    name: "Thiên thần tình yêu",
+    artist: "RICKY STAR x T.R.I",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Giấc mơ rất thơ",
+    artist: "Mer ft ThaiSon!",
+    img: "music-4",//gtrt
+    src: "music-4"
+  },
+  {
+    name: "Gu",
+    artist: "Freaky ft. Seachains",
+    img: "music-5",// Gu
+    src: "music-5"
+  },
+  {
+    name: "thichthich",
+    artist: "phương ly",
+    img: "music-6",//thichx2
+    src: "music-6"
+  },
+  {
+    name: "Em Là Nhất <3",
+    artist: "kis x Hoàng KayLee x Yahy",
+    img: "music-7",//eln
+    src: "music-7"
+  },
+  {
+    name: "Dịu dàng là ngày em đến :3",
+    artist: "ERIK",
+    img: "music-8",//dd
+    src: "music-8"
+  },
+  {
+    name: "Người Em Cố Đô",
+    artist: "Rum x Đaa x Toann",
+    img: "music-10",//ngecd
+    src: "music-10"
+  }
+];
